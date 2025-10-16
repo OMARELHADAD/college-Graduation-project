f17b3645903d2d09ac2fd1467f54af53a8e99410
omar elhadad <omarelhadad1711@gmail.com>
2025-10-16 23:24:57 +0300

    created the home page slider files and added some dummy data in the data file

---
+++ Frontend/src/components/catCard/CatCart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./CatCard.scss"
+
+const CatCard = () => {
+return (
+    <div className='catCard'>
+        <div className="contaier"></div>
+    </div>
+)
+}
+
+export default CatCard
